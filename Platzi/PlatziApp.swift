9b42e52844b84ec757d0f90bf753dbdfd470298f
Jose Valentin Abundo Hernandez <valentin.abundo@bambu-mobile.com>
2022-05-18 12:36:01 -0500

    Initial Commit

---
+++ Platzi/PlatziApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatziApp.swift
+//  Platzi
+//
+//  Created by Developer on 18/05/22.
+//
+
+import SwiftUI
+
+@main
+struct PlatziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
